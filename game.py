3358b9f1c03985bd3237dd3df4850a98a0deeca0
vitorueno <vitoruenootto@gmail.com>
2020-03-15 17:53:05 -0300

    add game template

---
+++ game.py
@@ -0,0 +1,44 @@
+import arcade
+from var import *
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        arcade.start_render()
+
+    def on_update(self, delta_time):
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        pass
+
+
+def main():
+    """ Main method """
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
