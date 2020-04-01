fcc3bb6206b76a420148af3510116cef0c4b7446
vitorueno <vitoruenootto@gmail.com>
2020-03-31 22:39:11 -0300

    create 'choose your side' screen

--- app/constants.py
+++ app/constants.py
@@ -1,13 +1,18 @@
 #screen 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Cold War"
+SCREEN_TITLE = 'Cold War'
 
-CHERNOBYL_FONT = "app/fonts/Chernobyl.otf"
+CHERNOBYL_FONT = 'app/fonts/Chernobyl.otf'
+OLD_FONT1 = 'app/fonts/OldNewspaperTypes.ttf'
+OLD_FONT2 = 'app/fonts/TravelingTypewriter.otf'
 
 #file location
-BG_MENU = "app/img/background/menu.png"
-BTN_NORMAL = "app/img/buttons/button_normal.png"
-BTN_HOVER = "app/img/buttons/button_hover2.png"
-BTN_CLICKED = "app/img/buttons/button_locked.png"
-BTN_LOCKED = "app/img/buttons/button_locked.png"
+BG_MENU = 'app/img/background/menu.png'
+BG_CHOOSE_SIDE = 'app/img/background/choose_side.png'
+EUA_SIDE = 'app/img/background/eua.png'
+URSS_SIDE = 'app/img/background/urss.png'
+BTN_NORMAL = 'app/img/buttons/button_normal.png'
+BTN_HOVER = 'app/img/buttons/button_hover2.png'
+BTN_CLICKED = 'app/img/buttons/button_locked.png'
+BTN_LOCKED = 'app/img/buttons/button_locked.png'
--- app/game.py
+++ app/game.py
@@ -27,7 +27,7 @@ class Button(TextButton):
 
 class QuitButton(TextButton):
     def __init__(self, view, x=0, y=0, width=100, height=40, 
-                 text="Quit",font_size=20,font_name=CHERNOBYL_FONT,
+                 text="Sair",font_size=20,font_name=CHERNOBYL_FONT,
                  font_color=arcade.color.WHITE,face=arcade.color.GRAY, 
                  highlight=arcade.color.WHITE,shadow=arcade.color.BLACK,
                  btn_height=7):
@@ -44,6 +44,7 @@ class QuitButton(TextButton):
             self.pressed = False
             arcade.close_window()
 
+
 class Menu(arcade.View):
     def on_show(self):
         arcade.set_background_color(arcade.color.RED_DEVIL)
@@ -53,37 +54,66 @@ class Menu(arcade.View):
     
     def set_buttons(self):
         self.button_list.append(Button(self, Choose_side(), SCREEN_WIDTH/2,
-                                SCREEN_HEIGHT/2 - 50, 110, 50, "Play", highlight=arcade.color.DARK_GRAY,
+                                SCREEN_HEIGHT/2 - 50, 110, 50, "Jogar", 
+                                highlight=arcade.color.DARK_GRAY,
                                 shadow=arcade.color.DARK_LIVER))
+        
         self.button_list.append(QuitButton(self, SCREEN_WIDTH/2,
-                                SCREEN_HEIGHT/2 - 150, 110, 50,highlight=arcade.color.DARK_GRAY,
+                                SCREEN_HEIGHT/2 - 150, 110, 50,
+                                highlight=arcade.color.DARK_GRAY,
                                 shadow=arcade.color.DARK_LIVER))
             
     def on_draw(self):
         arcade.start_render()
-        scale = SCREEN_WIDTH / self.background.width
-        arcade.draw_lrwh_rectangle_textured(0, 0,
-                                            SCREEN_WIDTH, SCREEN_HEIGHT,
+        arcade.draw_lrwh_rectangle_textured(0, 0,SCREEN_WIDTH, SCREEN_HEIGHT,
                                             self.background,0,255)
         super().on_draw()
-        #draw background
         
         
 class Choose_side(arcade.View):
     def on_show(self):
         arcade.set_background_color(arcade.color.BLUE_SAPPHIRE)
+        self.background = arcade.load_texture(BG_CHOOSE_SIDE)
+        self.side = None
+        self.eua = arcade.Sprite(EUA_SIDE,center_x=SCREEN_WIDTH/2 + 2,center_y=SCREEN_HEIGHT/2 - 46)
+        self.urss = arcade.Sprite(URSS_SIDE,center_x=SCREEN_WIDTH/2 - 1,center_y=SCREEN_HEIGHT/2 - 46)
+        self.button_list.append(Button(self, Menu(), SCREEN_WIDTH/2 - 280,
+                                30, 160, 40, "Voltar ao Menu", 15, 
+                                highlight=arcade.color.DARK_GRAY,
+                                shadow=arcade.color.DARK_LIVER))
 
+    def set_mission_button(self):
+        self.button_list.append(Button(self, Mission(), SCREEN_WIDTH/2 + 280,
+                                30, 160, 40, "Primeira missão", 15, 
+                                highlight=arcade.color.DARK_GRAY,
+                                shadow=arcade.color.DARK_LIVER))
+        
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Choose your side", SCREEN_WIDTH/2, SCREEN_HEIGHT/2,
-                         arcade.color.BLACK, font_size=50, anchor_x="center")
-        arcade.draw_text("Click to advance.", SCREEN_WIDTH/2, SCREEN_HEIGHT/2-75,
-                         arcade.color.GRAY, font_size=20, anchor_x="center")
-
+        arcade.draw_lrwh_rectangle_textured(0, 0,SCREEN_WIDTH, SCREEN_HEIGHT,
+                                            self.background,0,255)
+        if self.side is not None:
+            arcade.draw_text(f'Lado: {self.side}',SCREEN_WIDTH/2 ,20,arcade.color.WHITE,20,
+                             anchor_x="center",font_name=OLD_FONT1)
+            if self.side == 'EUA':
+                self.eua.draw()
+            else:
+                self.urss.draw()
+        super().on_draw()
+    
     def on_mouse_press(self, _x, _y, _button, _modifiers):
-        mission = Mission()
-        self.window.show_view(mission)
-        
+        point = (_x,_y)
+        if _button == arcade.MOUSE_BUTTON_LEFT:
+            if self.urss.collides_with_point(point) and not \
+                self.eua.collides_with_point(point):
+                self.side = 'EUA'
+                self.set_mission_button()
+            elif not self.urss.collides_with_point(point) and \
+                self.eua.collides_with_point(point) :
+                self.side = 'URSS'
+                self.set_mission_button()
+        super().on_mouse_press(_x,_y,_button,_modifiers)
+    
         
 class Mission(arcade.View):
     def on_show(self):
